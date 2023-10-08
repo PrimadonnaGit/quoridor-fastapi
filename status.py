cd7a61f4a83c31300678aa4c3b9f311ea7d6e522
bibiane <bibiane@daangn.com>
2023-10-08 15:24:37 +0900

    add missing file

---
+++ status.py
@@ -0,0 +1,13 @@
+STATUS_CODE = {
+    100: "room is full",
+    101: "room does not exist",
+    102: "connected to room",
+    200: "not your turn",
+    201: "invalid move",
+    202: "player has left the game. Start a new game",
+    300: "game not started",
+    301: "game started",
+    302: "game ended",
+    400: "connection lost",
+    500: "internal server error",
+}
